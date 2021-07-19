63a73ebbd877b863d19a6eb1ceadf64c5df6d2b0
Lucas <spirolucas@hotmail.com>
2021-07-19 16:28:43 -0300

    commit inicial

---
+++ meu-app/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'disciplina',
+    loadChildren: () => import('./disciplina/disciplina.module').then( m => m.DisciplinaPageModule)
+  },
+  {
+    path: 'disciplina/:codigo',
+    loadChildren: () => import('./disciplina/disciplina.module').then( m => m.DisciplinaPageModule)
+  },
+ 
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ meu-app/src/app/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Disciplina } from '../model/disciplina';
+import { DisciplinaService } from '../service/disciplina.service';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  public disciplinas: Disciplina[]=[];
+  public totalCH: number;
+  public cr: number;
+
+  constructor(private disciplinaService: DisciplinaService) {}
+
+  ngOnInit(){
+    this.disciplinaService.atualizarDisciplinas();
+    this.listarDisciplinas();
+  }
+
+  listarDisciplinas(){
+    this.disciplinaService.disciplinas.
+    subscribe(
+      (result)=>{
+        this.disciplinas = result;
+        this.totalCH=this.disciplinas.reduce((total,d) => total + d.cargaHoraria, 0);
+        const PCH: number=this.disciplinas.reduce((pch,d) => pch+ d.cargaHoraria * d.nota, 0);
+        this.cr=PCH/this.totalCH;
+    });
+  }
+}
